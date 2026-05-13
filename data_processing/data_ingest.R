1593214b5f9b158a89f39c7ee077804e1baf5e06
ehill-iolani <ehill@iolani.org>
2026-05-13 11:04:45 -1000

    remove duplicate library calls, key assignment, and fix typo

--- data_processing/data_ingest.R
+++ data_processing/data_ingest.R
@@ -1,22 +1,8 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(stringr)
-library(plotly)
-library(shinydashboard)
-library(shinyBS)
-library(DT)
-library(htmlwidgets)
-library(leaflet)
-library(tidyr)
-
 ####################
 ### Pulling data ###
 ####################
-# Read in the API key, basename, and table name from the .Renviron file
-readRenviron("~")
 
-# Verifies the all the necessary environment variables are set
+# Verifies all necessary environment variables are set
 if (Sys.getenv("AIRTABLE_API_KEY") == "")
   print("API key not found") else print("API key found")
 
@@ -35,17 +21,14 @@ if (Sys.getenv("AIRTABLE_ORGANIZATIONDB_NAME") == "")
 if (Sys.getenv("AIRTABLE_FISHDB_NAME") == "")
   print("Fish table name not found") else print("Fish table name found")
 
-# Pulls data from Airtable
-key <- Sys.getenv("AIRTABLE_API_KEY")
-base <- Sys.getenv("AIRTABLE_BASE_NAME")
-l3_table_name <- Sys.getenv("AIRTABLE_L3DB_NAME")
-site_table_name <- Sys.getenv("AIRTABLE_SITEDB_NAME")
-organziation_table_name <- Sys.getenv("AIRTABLE_ORGANIZATIONDB_NAME")
-fish_table_name <- Sys.getenv("AIRTABLE_FISHDB_NAME")
-key <- Sys.getenv("AIRTABLE_API_KEY")
-record_id <- NULL
+key                   <- Sys.getenv("AIRTABLE_API_KEY")
+base                  <- Sys.getenv("AIRTABLE_BASE_NAME")
+l3_table_name         <- Sys.getenv("AIRTABLE_L3DB_NAME")
+site_table_name       <- Sys.getenv("AIRTABLE_SITEDB_NAME")
+organization_table_name <- Sys.getenv("AIRTABLE_ORGANIZATIONDB_NAME")
+fish_table_name       <- Sys.getenv("AIRTABLE_FISHDB_NAME")
 
-ldat <- get_airtable_records(base, l3_table_name, key, record_id)
-sdat <- get_airtable_records(base, site_table_name, key, record_id)
-odat <- get_airtable_records(base, organziation_table_name, key, record_id)
-fdat <- get_airtable_records(base, fish_table_name, key, record_id)
+ldat <- get_airtable_records(base, l3_table_name, key)
+sdat <- get_airtable_records(base, site_table_name, key)
+odat <- get_airtable_records(base, organization_table_name, key)
+fdat <- get_airtable_records(base, fish_table_name, key)
